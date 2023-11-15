8f0a9a2ba7dfbdb970814b21232dc21d13068a44
alex bu <4lex.bu@gmail.com>
2023-11-15 21:36:17 +0400

    Unskippable ads & skip clickers

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,17 @@
 export const removeAds = () => {
   const adSkip = document.querySelector('.ytp-ad-skip-button') as HTMLButtonElement;
   if (adSkip) adSkip.click();
+  const modernAdSkip = document.querySelector('.ytp-ad-skip-button-modern') as HTMLButtonElement;
+  if (modernAdSkip) modernAdSkip.click();
+  const unskippableAd = document.querySelector('.ytp-ad-preview-container');
+  if (unskippableAd) {
+    const player = document.querySelector('.html5-main-video') as HTMLVideoElement;
+    if (player) player.currentTime = 999;
+  }
   const adRenderer = document.querySelector('.ytd-ad-slot-renderer');
   if (adRenderer) adRenderer.remove();
+  const playerAds = document.querySelector('#player-ads');
+  if (playerAds) playerAds.remove();
 };
 
 export const init = () => {
